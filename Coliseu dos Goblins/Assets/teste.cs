2c16dd754a7d271f200133a0a821723eda462cfb
Janderson Araujo <j.a.brito@live.com>
2015-10-23 00:07:34 -0300

    comit assets novos

---
+++ Coliseu dos Goblins/Assets/teste.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teste : MonoBehaviour {
+
+	public NavMeshAgent nav;
+	public Transform destino;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.destination = destino.position;
+	}
+
+	void OnTriggerEnter(){
+
+	}
+}
